6c78353608dfb63964bd691c816fec0197d83c73
Charles Jackson <cpjackson572@gmail.com>
2019-12-04 15:49:49 -0600

    added new JS files

---
+++ src/components/messages/MessageForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import MessagesManager from '../../modules/MessagesManager'
+
+class MessageForm extends Component {
+    state = {
+        message: "",
+        timeStamp: "",
+        loadingStatus: false,
+    }
+
+    
+}
+
+export default MessageForm
