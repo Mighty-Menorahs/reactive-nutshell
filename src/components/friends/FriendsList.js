d1e47e9ae5a195ff96e90749f94bd1f1746f3f29
Melody Stern <melliemuse@gmail.com>
2019-12-05 12:09:51 -0600

    populated friends list component with Add Friend button and boolean value which toggles the display of the friends search input field

--- src/components/friends/FriendsList.js
+++ src/components/friends/FriendsList.js
@@ -2,21 +2,38 @@ import React, { Component } from 'react'
 
 class FriendsList extends Component {
     state = {
-
+        userId: "",
+        loggedInUserId: "",
+        showInputField: false
     }
 
     componentDidMount() {
 
     }
+    handleFieldChange = (event) => {
+        const stateToChange = {}
+        stateToChange[event.target.id] = event.target.value
+        this.setState(stateToChange)
+    }
     addFriend = () => {
+        this.setState({
+            showInputField: true,
+        })
 
     }
     render() {
-        return(
+        return (
             <>
-            <button
-            onClick={this.addFriend}
-            >Add New Friend</button>
+                <div>
+                    <button
+                        onClick={this.addFriend}
+                    >Add New Friend</button>
+                    <input 
+                    id="userId"
+                    hidden={! this.state.showInputField}
+                    onChange={this.handleFieldChange}
+                    />
+                </div>
             </>
         )
     }
